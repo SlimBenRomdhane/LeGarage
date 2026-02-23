f9759357b60fa7f2d93ef3259c3f977571ce58e4
SlimBenRomdhane <taf.slim.benromdhane@gmail.com>
2026-02-23 10:21:08 +0100

    PaymentMethodConfiguration

---
+++ LeGarage.Core/Configurations/PaymentMethodConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using LeGarage.Core.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LeGarage.Core.Configurations
+{
+    public class PaymentMethodConfiguration : BaseEntityConfiguration<PaymentMethod>
+    {
+        public override void Configure(EntityTypeBuilder<PaymentMethod> entity)
+        {
+            base.Configure(entity);
+            entity.HasIndex(pm => pm.MethodCode).IsUnique();
+            entity.Property(pm => pm.MethodCode).HasMaxLength(50);
+            entity.Property(pm => pm.Name).HasMaxLength(50);
+            entity.Property(pm => pm.Description).HasMaxLength(254);
+
+
+        }
+    }
+}
--- LeGarage.Core/Entities/PaymentMethod.cs
+++ LeGarage.Core/Entities/PaymentMethod.cs
@@ -1,15 +1,11 @@
-﻿
-using LeGarage.Core.Common;
+﻿using LeGarage.Core.Common;
 
 namespace LeGarage.Core.Entities;
 
 public class PaymentMethod : BaseEntity
 {
-    public int PaymentMethodId { get; set; }
-
+    public required string MethodCode { get; set; }
     public required string Name { get; set; }
-
-    public required string Description { get; set; }
-
+    public string? Description { get; set; }
     public ICollection<Payment> Payments { get; set; } = new List<Payment>();
 }
